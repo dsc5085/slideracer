128e63a3802c454a736914151d098d8827012192
David Chen <dchen5085@gmail.com>
2015-12-24 21:35:22 -0500

    initial

---
+++ desktop/src/dc/slideracer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dc.slideracer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dc.slideracer.SlideRacerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SlideRacerGame(), config);
+	}
+}

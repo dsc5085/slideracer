10e2dc99f257e2cbcddaeb586ae6d9d3fcdf54b9
David Chen <dchen5085@gmail.com>
2016-01-09 16:57:33 -0500

    Simple racer input; follows mouse directly

--- core/src/dc/slideracer/entitysystems/RacerInputSystem.java
+++ core/src/dc/slideracer/entitysystems/RacerInputSystem.java
@@ -21,30 +21,12 @@ public final class RacerInputSystem extends EntitySystem {
 	@Override
 	public final void updateEntity(final float delta, final Entity entity) {
 		if (entity.hasActive(RacerInputPart.class)) {
-			Vector2 startCoords;
-			// TODO: DOesn't take into account world zoom
-			float touchDeltaX = Gdx.input.getDeltaX() / unitConverter.getPixelsPerUnit();
+			Vector2 waypoint = unitConverter.toWorldCoords(Gdx.input.getX(), 0);
+			waypoint.y =  entity.get(TransformPart.class).getCenter().y;
 			WaypointsPart waypointsPart = entity.get(WaypointsPart.class);
-			Vector2 entityCenter = entity.get(TransformPart.class).getCenter();
-			if (waypointsPart.hasWaypoints() && isTouchDeltaAndWaypointDirectionSame(
-					touchDeltaX, waypointsPart, entityCenter.x)) {
-				startCoords = waypointsPart.getCurrentWaypoint();
-			} else {
-				startCoords = entityCenter;
-			}
 			waypointsPart.clearWaypoints();
-			Vector2 newWaypoint = startCoords.add(touchDeltaX, 0);
-			System.out.println(entityCenter);
-			System.out.println(newWaypoint);
-			waypointsPart.addWaypoint(newWaypoint);
+			waypointsPart.addWaypoint(waypoint);
 		}
-	}
-	
-	private boolean isTouchDeltaAndWaypointDirectionSame(final float touchDeltaX, final WaypointsPart waypointsPart, 
-			final float entityCenterX) {
-		float offsetToWaypoint = waypointsPart.getCurrentWaypoint().x - entityCenterX;
-		System.out.println(Math.signum(touchDeltaX) == Math.signum(offsetToWaypoint));
-		return Math.signum(touchDeltaX) == Math.signum(offsetToWaypoint);
 	}
 
 }

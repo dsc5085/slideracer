b8e547ceae85e54538bca257f6f42b633353fd41
David Chen <dchen5085@gmail.com>
2016-04-21 20:31:27 -0400

    Remove margin restrictions from terrain factory creation

--- core/src/dc/slideracer/level/TerrainFactory.java
+++ core/src/dc/slideracer/level/TerrainFactory.java
@@ -14,7 +14,6 @@ import dclib.geometry.VertexUtils;
 
 public class TerrainFactory {
 	
-	private static final float HORIZONTAL_MARGIN = 1;
 	private static final float EDGE_MAX_DEVIATION_X = 5; // TODO: Make this based off of player speed
 	private static final float EDGE_MAX_INCREASE_Y = 8;
 	
@@ -44,16 +43,14 @@ public class TerrainFactory {
 	private List<Vector2> createLeftCliffVertices() {		
 		List<Vector2> vertices = new ArrayList<Vector2>();
 		Rectangle bounds = level.getBounds();
-		Vector2 start = new Vector2(bounds.x + HORIZONTAL_MARGIN, bounds.y);
+		Vector2 start = new Vector2(bounds.x, bounds.y);
 		vertices.add(start);
 		while (true) {
 			Vector2 lastWaypoint = vertices.get(vertices.size() - 1);
 			float boundsTop = RectangleUtils.top(bounds);
 			float y = Math.min(lastWaypoint.y + MathUtils.random(EDGE_MAX_INCREASE_Y), boundsTop);
-			float minX = Math.max(lastWaypoint.x - EDGE_MAX_DEVIATION_X, bounds.x + HORIZONTAL_MARGIN);
-			float maxX = Math.min(lastWaypoint.x + EDGE_MAX_DEVIATION_X, 
-					RectangleUtils.right(bounds) - HORIZONTAL_MARGIN - racerSize.x);
-			float x = MathUtils.random(minX, maxX);
+			float edgeDeviationX = MathUtils.random(-EDGE_MAX_DEVIATION_X, EDGE_MAX_DEVIATION_X);
+			float x = lastWaypoint.x + edgeDeviationX;
 			vertices.add(new Vector2(x, y));
 			if (y >= boundsTop) {
 				return vertices;
@@ -64,18 +61,18 @@ public class TerrainFactory {
 	private List<Vector2> createRightCliffVertices(final List<Vector2> leftCliffVertices) {
 		List<Vector2> vertices = new ArrayList<Vector2>();
 		Rectangle bounds = level.getBounds();
-		float rightEdgeStartX = RectangleUtils.right(bounds) - HORIZONTAL_MARGIN;
-		float pathStartWidth = rightEdgeStartX - leftCliffVertices.get(0).x;
-		float boundsRight = RectangleUtils.right(bounds);
+		float pathStartWidth = RectangleUtils.right(bounds) - leftCliffVertices.get(0).x;
+		float maxX = Integer.MIN_VALUE;
 		for (Vector2 leftPoint : leftCliffVertices) {
 			float widthAlpha = (leftPoint.y - bounds.y) / bounds.height;
 			float width = Interpolation.linear.apply(pathStartWidth, racerSize.x, widthAlpha);
-			float x = Math.min(leftPoint.x + width, boundsRight - HORIZONTAL_MARGIN);
+			float x = leftPoint.x + width;
+			maxX = Math.max(x, maxX);
 			vertices.add(new Vector2(x, leftPoint.y));
 		}
-		Vector2 topRightCornerPoint = new Vector2(boundsRight, RectangleUtils.top(bounds));
+		Vector2 topRightCornerPoint = new Vector2(maxX, RectangleUtils.top(bounds));
 		vertices.add(topRightCornerPoint);
-		Vector2 bottomRightCornerPoint = new Vector2(boundsRight, bounds.y);
+		Vector2 bottomRightCornerPoint = new Vector2(maxX, bounds.y);
 		vertices.add(bottomRightCornerPoint);
 		return vertices;
 	}
